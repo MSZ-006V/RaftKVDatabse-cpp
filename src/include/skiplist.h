39feda3693562bb6554153c2ff8a6e03e2ca5c8d
MSZ-006NOC <msz006incubus@gmail.com>
2025-01-09 17:06:16 -0800

    update skiplist

--- src/include/skiplist.h
+++ src/include/skiplist.h
@@ -19,13 +19,17 @@ public:
     void setValue(V val);
 
     node<K, V> **forward;
-    int nodeLevel;
+    int node_level;
 
 private:
     K key;
     V value
 };
 
+// & 是Boost库对序列化操作符的重载，用于同时支持序列化和反序列化
+// 类似于标准I/O操作符<<和>>，根据传入的archive的类型（输入流或是输出流）
+// 自动决定执行序列化还是反序列化功能
+// & 是序列化流程的核心操作符，通过自动判断archive类型实现双向数据流
 template<typename K, typename V>
 class SkipListDump{
 public:
@@ -45,8 +49,40 @@ public:
 template<typename K, typename V>
 class SkipList{
 public:
-    SkipList(int level);
+    SkipList(int max_level);
     ~SkipList();
-private:
 
+    void displayList();
+
+    int getRandomLevel();
+    node<K, V> *createNode(K, V, int);
+
+    int insertElement(K, V);
+    bool searchElement(K, V &value);
+    void deleteElement(K);
+    void insertSetElement(K &, V &);
+
+    std::string dumpFile();
+    void loadFile(const std::string &dumpStr);
+
+    void clear(node<K, V> *);
+    int size();
+private:
+    void getKeyValuefromString(const std::string &str, std::string *key, std::string *value);
+    bool isValidString(const std::string &str);
+private:
+    std::mutex m_mtx;
+
+    // Maximum level of the skip list
+    int max_level;
+    // Current level of skip list
+    int skiplist_level;
+
+    node<K, V> *head;
+
+    std::ofstream file_writer;
+    std::ifstream file_reader;
+
+    // Skiplist current element count
+    int skiplist_element_count;
 };
--- src/skiplist.cpp
+++ src/skiplist.cpp
@@ -6,7 +6,7 @@ template<typename K, typename V>
 node<K, V>::node(K key, V val, int level){
     this->key = key;
     this->value = val;
-    this->nodeLevel = level;
+    this->node_level = level;
     this->forward = new node<K, V> *[level + 1];
 
     memset(this->forward, 0, sizeof(node<K, V> *) * (level + 1));
@@ -30,4 +30,92 @@ V node<K, V>::getVal() const{
 template<typename K, typename V>
 void node<K, V>::setValue(V val){
     this->value = val;
+}
+
+
+template<typename K, typename V>
+void SkipListDump<K, V>::insert(const node<K, V> &node){
+    keyDumpVT_.emplace_back(node.getKey());
+    valDumpVT_.emplace_back(node.getVal());
+}
+
+
+template<typename K, typename V>
+SkipList<K, V>::SkipList(int max_level){
+    this->max_level = max_level;
+    this->skiplist_level = 0;
+    this->skiplist_element_count = 0;
+
+    K k;
+    V v;
+    this->head = new node<K, V>(k, v, max_level);
+}
+
+template<typename K, typename V>
+SkipList<K, V>::~SkipList(){
+    // 首先关闭输入输出文件流，然后再执行析构函数
+    if(file_writer.is_open()) file_writer.close();
+    if(file_reader.is_open()) file_reader.close();
+
+    // 递归删除跳表
+    if(head->forward[0] != nullptr){
+        clear(head->forward[0]);
+    }
+    delete (head);
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::displayList(){
+
+}
+
+
+template<typename K, typename V>
+int SkipList<K, V>::getRandomLevel(){
+
+}
+
+template<typename K, typename V>
+node<K, V> *SkipList<K, V>::createNode(K k, V v, int level){
+
+}
+
+template<typename K, typename V>
+int SkipList<K, V>::insertElement(K k, V v){
+
+}
+
+template<typename K, typename V>
+bool SkipList<K, V>::searchElement(K k, V &v){
+
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::deleteElement(K k){
+
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::insertSetElement(K &k, V &v){
+
+}
+
+template<typename K, typename V>
+std::string SkipList<K, V>::dumpFile(){
+
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::loadFile(const std::string &dumpStr){
+
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::clear(node<K, V> *node){
+
+}
+
+template<typename K, typename V>
+int SkipList<K, V>::size(){
+
 }
